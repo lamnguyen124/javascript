d641699bc0edb6b6da63eb62dda1f21c45b489bb
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-10-09 15:45:29 -0500

    basic functions

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,18 @@
+
+//Performing a task
 function greet(name, lastName) {
     console.log("Hey " + name + ' ' + lastName);
 }
 
+//calculate a value
+function square(number) {
+    return number * number;
+}
+
 greet('Ally', 'Baba');
+
+ // let number = square(2);
+ // console.log(number);
+
+console.log(square(2));
 
