0a5d4a51c47f451ce34b1a5de21632bb2f88f7e8
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-10-09 14:55:25 -0500

    refresing js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+console.log("Hello Noob");
+
+let name = "Lam";
+console.log(name);
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let name = "Ally"; // String Literal
+let age = 1; //  Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined;
+let selectedColor = null;
---
+++ js/object.js
@@ -0,0 +1,14 @@
+
+let dog = {
+    name : "Ally",
+    age: 1
+};
+
+// Dot Notation
+dog.name = "Happy";
+
+//Bracket Notation
+let selection = 'name';
+dog['selection'] = "Kiwi";
+
+console.log(dog.name);
