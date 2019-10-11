bc3bca648d42008a38dcb9a1fea35538596c72a1
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-10-11 14:25:10 -0500

    progressing with exercise

---
+++ js/exercise2.js
@@ -0,0 +1,18 @@
+function addingTwoNumbers(a,b){
+    return a + b;
+}
+
+console.log(addingTwoNumbers(6, 4));
+
+function max(a, b) {
+    // if (a > b)
+    //     return a;
+    // else
+    //     return b;
+    return (a > b) ? a : b;
+}
+
+let number = max(1, 2);
+console.log(number);
+
+function isLandscape(wid)
